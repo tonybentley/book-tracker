2654ce483e2362dfd7e870ecf82cf589da0467e2
Tony Bentley <tonybentley@Tonys-MacBook-Pro-3.local>
2023-12-21 10:32:42 -0800

    Adding database provider, book and author resources, typeorm, request validation and transformation, mysql runtime via docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthorModule } from './author/author.module';
+import { databaseProviders } from './database/database.providers';
 
 @Module({
-  imports: [],
+  imports: [BookModule, AuthorModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: console });
+  app.useGlobalPipes(new ValidationPipe({}));
+  const config = new DocumentBuilder()
+    .setTitle('Book Tracker')
+    .setDescription('The Book API')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
