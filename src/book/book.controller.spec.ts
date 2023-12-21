29ba046661c9c917de49033fed7a3bbf8e14aabd
Tony Bentley <tonybentley@Tonys-MacBook-Pro-3.local>
2023-12-21 12:19:43 -0800

    book service and controller unit tests

--- src/book/book.controller.spec.ts
+++ src/book/book.controller.spec.ts
@@ -1,20 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+const book = {
+  id: 1,
+  title: 'The Great Gatsby',
+  genre: 'Fiction',
+  pageCount: 208,
+  releaseDate: new Date('1925-04-10:00:00.000Z'),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  authorId: 1,
+  author: {
+    id: 1,
+    firstName: '',
+    lastName: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    books: [],
+  },
+};
+const bookId = book.id.toString();
 
 describe('BookController', () => {
   let controller: BookController;
+  let service: BookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BookController],
-      providers: [BookService],
+      providers: [
+        {
+          provide: BookService,
+          useValue: {
+            remove: jest.fn(),
+            update: jest.fn(),
+            findOne: jest.fn(),
+            findAll: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BookController>(BookController);
+    service = module.get<BookService>(BookService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new book', async () => {
+    const createBookDto: CreateBookDto = book;
+    const createdBook = Object.assign(book, createBookDto);
+    jest.spyOn(service, 'create').mockResolvedValue(book);
+
+    const result = await controller.create(createBookDto);
+
+    expect(result).toEqual(createdBook);
+  });
+
+  it('should return all books', async () => {
+    const books = [book];
+    jest.spyOn(service, 'findAll').mockResolvedValue(books);
+
+    const result = await controller.findAll(null, null);
+
+    expect(result).toEqual(books);
+  });
+
+  it('should return a book by id', async () => {
+    jest.spyOn(service, 'findOne').mockResolvedValue(book);
+    const result = await controller.findOne(bookId);
+    expect(result).toEqual(book);
+  });
+
+  it('should update a book', async () => {
+    const updateBookDto: UpdateBookDto = {
+      title: 'Updated Book',
+    };
+    const updatedBook = Object.assign(book, updateBookDto);
+    jest.spyOn(service, 'update').mockResolvedValue(updatedBook);
+
+    const result = await controller.update(bookId, updateBookDto);
+
+    expect(result).toEqual(updatedBook);
+  });
+
+  it('should remove a book', async () => {
+    jest.spyOn(service, 'remove').mockResolvedValue(book);
+    const result = await controller.remove(bookId);
+
+    expect(result).toEqual(book);
   });
 });
--- src/book/book.service.spec.ts
+++ src/book/book.service.spec.ts
@@ -1,18 +1,89 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities/book.entity';
 
+const book = {
+  title: 'The Great Gatsby',
+  genre: 'Fiction',
+  pageCount: 208,
+  year: 1925,
+  releaseDate: new Date('1925-04-10:00:00.000Z'),
+  id: 1,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  author: {
+    id: 1,
+    firstName: '',
+    lastName: '',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    books: [],
+  },
+};
 describe('BookService', () => {
   let service: BookService;
+  let bookRepository: Partial<Repository<Book>> = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      providers: [
+        BookService,
+        { provide: 'BOOK_REPOSITORY', useValue: bookRepository },
+      ],
     }).compile();
 
     service = module.get<BookService>(BookService);
+    bookRepository = module.get('BOOK_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a book', async () => {
+    const createBookDto: CreateBookDto = {
+      title: book.title,
+      pageCount: book.pageCount,
+      genre: book.genre,
+      releaseDate: new Date(book.releaseDate),
+      authorId: book.author.id,
+    };
+    bookRepository.create = jest.fn().mockReturnValue(book);
+    bookRepository.save = jest.fn().mockReturnValue(book);
+
+    const result = await service.create(createBookDto);
+    expect(bookRepository.create).toHaveBeenCalledWith(createBookDto);
+    expect(bookRepository.save).toHaveBeenCalledWith(book);
+    expect(result).toEqual(book);
+  });
+
+  it('should update a book', async () => {
+    const bookId = 1;
+    const updateBookDto: UpdateBookDto = {
+      title: 'Updated Title',
+      pageCount: 300,
+      genre: 'Updated Genre',
+      releaseDate: new Date('2022-01-01'),
+      authorId: 2,
+    };
+    const updatedBook: Book = {
+      ...book,
+      ...updateBookDto,
+      updatedAt: new Date(),
+    };
+    bookRepository.findOne = jest.fn().mockResolvedValue(book);
+    bookRepository.save = jest.fn().mockResolvedValue(updatedBook);
+    const result = await service.update(bookId, updateBookDto);
+    expect(result).toEqual(updatedBook);
   });
 });
